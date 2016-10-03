1ba7c411d4b69be78864a7d7f10a351e8053d586
simonpatrat <simon.patrat@gmail.com>
2016-10-02 20:36:03 -0400

    Added bounding box.

--- state0.js
+++ state0.js
@@ -2,33 +2,54 @@ var demo = {};
 var centerX = 1500 / 2;
 var centerY = 1000 / 2;
 var adam;
-var speed = 4;
+var speed = 5;
 
 demo.state0 = function() {};
 demo.state0.prototype = {
     preload: function() {
         game.load.image('adam', 'assets/sprites/adam.png');
+        game.load.image('tree', 'assets/backgrounds/treeBG.png');
+
     },
     create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         console.log('State 0');
         game.stage.backgroundColor = '#80FF80';
         game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
         addChangeStateEventlisteners();
+        game.world.setBounds(0,0, 2813, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
+        var treeBG = game.add.sprite(0,0, 'tree');
+
+        // Adding character sprite
         adam = game.add.sprite(centerX, centerY, 'adam');
         adam.anchor.setTo(0.5, 0.5);
+        adam.scale.setTo(0.7, 0.7);
+        game.physics.enable(adam);
+        adam.body.collideWorldBounds = true;
+
+        // Game camera follows character
+        game.camera.follow(adam);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
 
     },
     update: function() {
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            adam.scale.setTo(0.7, 0.7);
             adam.x += speed;
         } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            adam.scale.setTo(-0.7, 0.7);
             adam.x -= speed;
         }
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
             adam.y -= speed;
+
+            if(adam.y < 395) {
+               adam.y = 395;
+            }
+
         } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
             adam.y += speed;
         }
